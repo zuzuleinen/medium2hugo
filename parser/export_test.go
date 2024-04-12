811080f5813cd83dc4eee507882470ee0d532c55
Andrei Boar <andrey.boar@gmail.com>
2024-04-12 10:10:04 +0300

    update test

--- parser/export_test.go
+++ parser/export_test.go
@@ -8,20 +8,20 @@ import (
 
 var images = []struct {
 	url      string
-	filename string
+	expected string
 }{
 	{
 		url:      "https://miro.medium.com/v2/resize:fit:640/1*-KZONqGNNwqPJ4Bmf70o-Q.png",
-		filename: "1*-KZONqGNNwqPJ4Bmf70o-Q.png",
+		expected: "1*-KZONqGNNwqPJ4Bmf70o-Q.png",
 	},
 	{
 		url:      "https://miro.medium.com/v2/resize:fit:640/1*M7AlXdGEfds9uD8fgkU6qw.png",
-		filename: "1*M7AlXdGEfds9uD8fgkU6qw.png",
+		expected: "1*M7AlXdGEfds9uD8fgkU6qw.png",
 	},
 }
 
 func TestExtractFilename(t *testing.T) {
 	for _, v := range images {
-		assert.Equal(t, v.filename, extractFilename(v.url))
+		assert.Equal(t, v.expected, extractFilename(v.url))
 	}
 }
